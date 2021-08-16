bfce6b6085c76f16039648ac8f38fc6ce2424ac7
unknown <lgarridoregife@gmail.com>
2021-08-16 11:34:33 +0200

    Lineal and hysteresis popup initialization and mode.json generation in config/tmp

--- src/interface/py/PopUps/HysteresisPopup.py
+++ src/interface/py/PopUps/HysteresisPopup.py
@@ -4,28 +4,51 @@ import json
 from pathlib import Path
 from kivy.uix.popup import Popup
 from kivy.properties import ObjectProperty
+from kivy.properties import NumericProperty
 from ..PopUps.ErrorWarning import ErrorWarningPopup
 
 
 class HysteresisPopup(Popup):
     id_hysteresis_popup = ObjectProperty(None)
+    param_v_1_init = NumericProperty(0.0)
+    param_v_2_init = NumericProperty(0.0)
+    param_points_init = NumericProperty(0.0)
+    param_speed_init = NumericProperty(0.0)
+    param_delay_init = NumericProperty(0.0)
+    param_cmpl_init = NumericProperty(0.0)
+    param_light_power_init = NumericProperty(0.0)
+    param_area_init = NumericProperty(0.0)
 
-    def make_hysteresis_dict(self):
-        params_hysteresis_lst = [
-            'param_v_start',
-            'param_v_stop',
-            'param_points',
-            'param_speed',
-            'param_delay',
-            'param_cmpl',
-            'param_light_power',
-            'param_area'
+    def __init__(self, **kwargs):
+        super(HysteresisPopup, self).__init__(**kwargs)
+        self.params_hysteresis_lst = [
+            'v_1',
+            'v_2',
+            'points',
+            'speed',
+            'delay',
+            'cmpl',
+            'light_power',
+            'area'
         ]
 
+        path = str(Path(__file__ + '/../../../../config/tmp/mode.json').resolve())
+        f = open(path)
+        init_dict = json.load(f)
+        self.param_v_1_init = init_dict['config']['v_1']
+        self.param_v_2_init = init_dict['config']['v_2']
+        self.param_points_init = int(init_dict['config']['points'])
+        self.param_speed_init = init_dict['config']['speed']
+        self.param_delay_init = init_dict['config']['delay']
+        self.param_cmpl_init = init_dict['config']['cmpl']
+        self.param_light_power_init = init_dict['config']['light_power']
+        self.param_area_init = init_dict['config']['area']
+
+    def make_hysteresis_dict(self):
         params_hysteresis = dict()
         trigger = True
-        for par in params_hysteresis_lst:
-            the_reference = self.ids[par]
+        for par in self.params_hysteresis_lst:
+            the_reference = self.ids['param_' + par]
             params_hysteresis[par] = float(the_reference.text or 0)
             the_reference.text = the_reference.text
             if the_reference.text == '':
@@ -44,11 +67,11 @@ class HysteresisPopup(Popup):
                 break
 
         while trigger:
-            dict_hysteresis = {'mode': 'Hysteresis',
+            dict_hysteresis = {'mode': 'hysteresis',
                                'config': params_hysteresis}
 
             # Create mode.json
-            mode_path = Path(__file__ + '/../../../../measures/mode.json')
+            mode_path = Path(__file__ + '/../../../../config/tmp/mode.json')
             mode_path = mode_path.resolve()
             with open(mode_path, 'w') as f:
                 json.dump(dict_hysteresis, f, indent=2)
