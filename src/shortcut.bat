f849c1c356d3d1a758dabc2f6a795514dcd907ae
unknown <lgarridoregife@gmail.com>
2021-08-30 11:40:13 +0200

    Changes in shortcut.

--- src/shortcut.bat
+++ src/shortcut.bat
@@ -1,10 +1,11 @@
+#!/bin/bash
 @echo off
 cd .\
 echo Set oWS = WScript.CreateObject("WScript.Shell") > CreateShortcut.vbs
-echo sLinkFile = "%userprofile%\Desktop\SolarCellAnalyzer.lnk" >> CreateShortcut.vbs
+echo sLinkFile = "%userprofile%\Desktop\Solar Cell Analyzer.lnk" >> CreateShortcut.vbs
 echo Set oLink = oWS.CreateShortcut(sLinkFile) >> CreateShortcut.vbs
-echo oLink.TargetPath = ".\run_script.bat" >> CreateShortcut.vbs
-echo oLink.WorkingDirectory = ".\" >> CreateShortcut.vbs
+echo oLink.TargetPath = "D:\Proyectos\solar_cell_analyzer\src\run_script.bat" >> CreateShortcut.vbs
+echo oLink.WorkingDirectory = "D:\Proyectos\solar_cell_analyzer\src" >> CreateShortcut.vbs
 echo oLink.Description = "Solar Cell Analyzer" >> CreateShortcut.vbs
 echo oLink.IconLocation = "D:\Proyectos\solar_cell_analyzer\src\interface\icon3_EcT_icon.ico" >> CreateShortcut.vbs
 echo oLink.Save >> CreateShortcut.vbs
