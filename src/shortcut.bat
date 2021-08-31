2cb7bf4977d5913448a3ab86bb28baf9b95dbc3d
unknown <lgarridoregife@gmail.com>
2021-08-31 12:49:25 +0200

    Create icon and launch app without cmd.

--- src/shortcut.bat
+++ src/shortcut.bat
@@ -4,7 +4,7 @@ cd .\
 echo Set oWS = WScript.CreateObject("WScript.Shell") > CreateShortcut.vbs
 echo sLinkFile = "%userprofile%\Desktop\Solar Cell Analyzer.lnk" >> CreateShortcut.vbs
 echo Set oLink = oWS.CreateShortcut(sLinkFile) >> CreateShortcut.vbs
-echo oLink.TargetPath = "D:\Proyectos\solar_cell_analyzer\src\run_script.bat" >> CreateShortcut.vbs
+echo oLink.TargetPath = "D:\Proyectos\solar_cell_analyzer\src\run_no_prompt.vbs" >> CreateShortcut.vbs
 echo oLink.WorkingDirectory = "D:\Proyectos\solar_cell_analyzer\src" >> CreateShortcut.vbs
 echo oLink.Description = "Solar Cell Analyzer" >> CreateShortcut.vbs
 echo oLink.IconLocation = "D:\Proyectos\solar_cell_analyzer\src\interface\icon3_EcT_icon.ico" >> CreateShortcut.vbs
